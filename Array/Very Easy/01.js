d8664af37b1e47fd28953d71733a51d1d9e5ad92
shihab <game123ahmed@gmail.com>
2022-03-13 17:24:32 +0600

    array new problem solved

---
+++ Array/Very Easy/01.js
@@ -0,0 +1,23 @@
+// Return the First Element in an Array
+// Create a function that takes an array containing only numbers and return the first element.
+
+// Examples
+// getFirstValue([1, 2, 3]) ➞ 1
+
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+
+
+
+function getFirstValue(arr){
+    return arr[0]
+
+}
+
+
+let result = getFirstValue([1,2,3])
+console.log(result);
+
+let result2 = getFirstValue([89,23,33])
+console.log(result2);
---
+++ object/very Easy/01.js
@@ -0,0 +1,35 @@
+//  =================================  very Easy  =================================================================
+// ----------------------------------- 01 -------------------------------------------------------------------------
+// Upvotes vs Downvotes
+
+
+// Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+// Examples
+// getVoteCount({ upvotes: 13, downvotes: 0 }) ➞ 13
+
+// getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
+
+// getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
+
+
+
+
+function getVoteCount({x,y}){
+  
+}
+
+getVoteCount({ upvotes:13,downvotes:0 })
+
+
+
+// let obj = {
+//     x:12,
+//     y:0,
+
+//     dow(){
+//         console.log(this.x - this.y);
+//     }
+
+// }
+// obj.dow()

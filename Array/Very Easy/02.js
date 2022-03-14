1d2ba25342e88c1dbc7720b2056a6296555c0a84
shihab <game123ahmed@gmail.com>
2022-03-14 17:19:08 +0600

    new array problem solved

---
+++ Array/Very Easy/02.js
@@ -0,0 +1,24 @@
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// // makePair(512124, 215) ➞ [512124, 215]
+
+
+
+function pairArray(a,b){
+
+    return [a,b]
+
+}
+
+
+let result = pairArray(22,10)
+console.log(result);
+
+let result2 = pairArray(51, 21)
+console.log(result2);

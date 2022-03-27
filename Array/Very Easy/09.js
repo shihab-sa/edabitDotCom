91adba74c029beb04b01188bc4219acd243fb056
shihab <game123ahmed@gmail.com>
2022-03-27 18:51:34 +0600

    new code added

---
+++ Array/Very Easy/09.js
@@ -0,0 +1,23 @@
+// Convert an Array to a String
+// Create a function that takes an array of numbers or letters and returns a string.
+
+// Examples
+// arrayToString([1, 2, 3, 4, 5, 6]) ➞ "123456"
+
+// arrayToString(["a", "b", "c", "d", "e", "f"]) ➞ "abcdef"
+
+// arrayToString([1, 2, 3, "a", "s", "dAAAA"]) ➞ "123asdAAAA"
+// Notes
+
+
+function arrayToString(arr){
+
+    let result = arr.join('')
+    return result
+
+}
+
+
+console.log(arrayToString([1, 2, 3, 4, 5, 6]));
+console.log(arrayToString(["a", "b", "c", "d", "e", "f"]));
+console.log(arrayToString([1, 2, 3, "a", "s", "dAAAA"]));
---
+++ Loops/Very Easy/07.js
@@ -0,0 +1,25 @@
+// Return Types
+// Create a function that takes an array and returns the types of values (data types) in a new array.
+
+// Examples
+// arrayValuesTypes([1, 2, "null", []])
+// ➞ ["number", "number", "string", "object"]
+
+// arrayValuesTypes(["214", true, false, 2, 2.15, [], null])
+// ➞ ["string", "boolean", "boolean", "number", "number", "object", "object"]
+
+// arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214])
+// ➞ ["number", "string", "string", "object", "object", "boolean", "number"]
+
+
+function arrayValuesTypes(arr){
+    let result = arr.map((type)=>{
+        return type && typeof Number && Array  && Object  && null  && undefined  && Boolean
+    })
+
+    return result
+
+}
+
+console.log(arrayValuesTypes([1, 2, "null", []]));
+console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]));

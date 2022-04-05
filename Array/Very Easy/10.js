0b531480d61a1f2ad14f5b3191351e8b20c1391c
shihab <game123ahmed@gmail.com>
2022-04-05 23:34:17 +0600

    new problems solved

---
+++ Array/Very Easy/10.js
@@ -0,0 +1,24 @@
+// Concatenating Two Integer Arrays
+// Create a function to concatenate two integer arrays.
+
+// Examples
+// concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+
+// concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+
+// concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+// Notes
+// Don't forget to return the result.
+// See Resources tab for more info.
+
+
+function concat(arrOne,arrTwo){
+
+    return arrOne.concat(arrTwo )
+
+}
+
+console.log(concat([1, 3, 5], [2, 6, 8]) );
+console.log(concat([7, 8], [10, 9, 1, 1, 2]));
+let result = concat([4, 5, 1], [3, 3, 3, 3, 3])
+console.log(result);

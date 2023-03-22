d5625fa82bcb777aa8ff4e3a6d5c52e18f071aa2
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-03-22 22:28:51 +0600

    new code added

---
+++ language fundamental/very_easy/01.js
@@ -0,0 +1,61 @@
+// How Edabit Works
+// This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+// function hello() {
+
+// }
+// All you have to do is type return "hello edabit.com" between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+
+// Notes
+// The returned string must be in all lowercase letters.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.How Edabit Works
+// This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+// function hello() {
+
+// }
+// All you have to do is type return "hello edabit.com" between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+
+// Notes
+// The returned string must be in all lowercase letters.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+
+
+// ==================================  way 01  =============================  
+console.log('way 01')
+function hello(){
+    return 'www.edabit.com'
+}
+console.log(hello());
+
+
+
+
+// ==================================  way 02  =============================  
+console.log('way 02')
+
+
+const hello2=()=>{
+    return 'www.edabit.com'
+}
+
+console.log(`The message is ${hello2()}`)
+
+
+
+
+// ==================================  way 03  =============================  
+console.log('way 03')
+
+
+
+let hello3 = function(){
+    return 'www.edabit.com'
+}
+
+
+let msg = hello3()
+console.log(`The Message is ${msg}`)

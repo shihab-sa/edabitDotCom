59a76ae84c5a0cfe4446a2a836c488daa4e5d01e
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-07-12 22:58:44 +0600

    new code added

---
+++ Logic/veryEasy/05.js
@@ -0,0 +1,33 @@
+// Return Negative
+// Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+
+// Examples
+// returnNegative(4) ➞ -4
+
+// returnNegative(15) ➞ -15
+
+// returnNegative(-4) ➞ -4
+
+// returnNegative(0) ➞ 0
+// Notes
+// N/A
+
+
+
+
+
+
+function returnNegative(number) {
+    if (number>0) {
+      return -number;
+    } else {
+      return number;
+    }
+  }
+  
+  // Example usage:
+  console.log(returnNegative(4)); // Output: -4
+  console.log(returnNegative(15)); // Output: -15
+  console.log(returnNegative(-4)); // Output: -4
+  console.log(returnNegative(0)); // Output: 0
+  

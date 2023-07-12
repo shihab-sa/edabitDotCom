9932f4eac086e3e6e785ee43d2908ea420392210
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-07-12 14:25:59 +0600

    new code added

---
+++ Logic/veryEasy/04.js
@@ -0,0 +1,10 @@
+// Minimal I: If Boolean Then Boolean
+// In this series we're going to see common redundancies and superfluities that make our code unnecessarily complicated and less readable, and we're going to learn how to avoid them.
+
+// In line with the spirit of the series, we can summarize the general rules of minimalist code in two simple principles:
+
+// Keep your code clean and readable.
+// While not violating the first principle: get rid of everything superfluous.
+
+
+// This Problem Statement wants to say how to write code readable and clear . Its like a blog post
--- String/Easy/02.js
+++ String/Easy/02.js
@@ -10,6 +10,34 @@
 // Notes
 // N/A
 
+step 01 
+
+// এই name ডাবল আছে পর পর, সেটা string আকারে, তাহলে যা করতে হবে,  indexOf() lastIndexOf() দিয়ে
+
+// function potatoes(str) {
+//   const lowercaseStr = str.toLowerCase();
+//   const target = "potato"
+
+//   // for single 
+//   let index = lowercaseStr.indexOf(target)
+
+//   //for duplicate 
+//   let index1 = lowercaseStr.lastIndexOf(target)
+//   //let count = 0 
+
+//   console.log('the indexof = ',index)
+//   console.log('the lastindex of is =',index1)
+// }
+
+// console.log(potatoes("potato"));  // ➞ 1
+// console.log(potatoes("potatopotato"));  // ➞ 2
+// console.log(potatoes("potatoapplepotato"));  //➞ 1
+
+
+
+
+Final Problem 
+
 function potatoes(str) {
     const lowercaseStr = str.toLowerCase();
     const target = 'potato';

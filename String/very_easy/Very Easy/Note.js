2854466aab48befccd40368b6894bca2e2bc3ca1
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-11-06 22:37:14 +0600

    new code added

--- String/very_easy/Very Easy/Note.js
+++ String/very_easy/Very Easy/Note.js
@@ -5,3 +5,8 @@ return str ? "true" : "false";
 // 02
 
 return `something ${str}`;
+
+
+// 03
+
+.length 
--- bugfixes/Note.js
+++ bugfixes/Note.js
@@ -4,3 +4,23 @@ a ** 3;
 // 02
 
 return `"${str}www.edabit.com"`;
+
+// 03
+
+function printArray(arr) {
+  let newArr = [];
+  for (let i = 1; i <= arr; i++) {
+    newArr.push(i);
+  }
+
+  return newArr;
+}
+
+console.log(printArray(3));
+
+// 04
+
+let temp = a;
+a = b;
+b = temp;
+return [a, b];
